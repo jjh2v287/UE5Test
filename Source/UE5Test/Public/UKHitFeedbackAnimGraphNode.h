33f7c07dbcc06bd6097a10a23174c8e910d19f5b
jjh2v2 <jjh2v2@gmail.com>
2024-04-08 10:22:18 +0900

    수정

--- Source/UE5Test/Public/UKHitFeedbackAnimGraphNode.h
+++ Source/UE5Test/Public/UKHitFeedbackAnimGraphNode.h
@@ -17,24 +17,24 @@ struct UE5TEST_API FUKHitFeedbackAnimNode : public FAnimNode_SkeletalControlBase
 
 public:
 	// 흔들려야하는 방향
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Translation, meta=(PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "HitFeedback", meta=(PinShownByDefault))
 	FVector Direction;
 	// 초기 진폭
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Links", meta = (PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HitFeedback", meta = (PinShownByDefault))
 	float InitialAmplitude = 10.0f;
 	// 감쇠율
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Links", meta = (PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HitFeedback", meta = (PinShownByDefault))
 	float Damping = 10.0f;
 	// 각속도
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Links", meta = (PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HitFeedback", meta = (PinShownByDefault))
 	float AngularVelocity = 60.0f;
 	// 흔들림 최대 시간
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Links", meta = (PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HitFeedback", meta = (PinShownByDefault))
 	float MaxTime = 0.2f;
 	// 시간 변수
 	float TimeElapsed = 0.0f;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Shake", meta = (PinShownByDefault))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HitFeedback", meta = (PinShownByDefault))
 	FName TargetBoneNames;
 
 	FName CopyTargetBoneNames;
