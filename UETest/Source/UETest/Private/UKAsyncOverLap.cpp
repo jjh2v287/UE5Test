aff45187186f2d5d72746fa1136fef99a15214d6
jjh2v2 <jjh2v2@gmail.com>
2024-06-23 02:29:46 +0900

    수정

---
+++ UETest/Source/UETest/Private/UKAsyncOverLap.cpp
@@ -0,0 +1,103 @@
+// Copyright Kong Studios, Inc. All Rights Reserved.
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(UKAsyncOverLap)
+
+#include "UKAsyncOverLap.h"
+
+#include "Engine/OverlapResult.h"
+
+void UUKAsyncOverLap::Activate()
+{
+	Super::Activate();
+
+	if (!OverlapDelegate.IsBound())
+	{
+		OverlapDelegate.BindUObject(this, &UUKAsyncOverLap::AsyncOverLapFinish);
+	}
+
+	FCollisionShape CollisionShape;
+	switch (AsyncOverLapInfo.ShapeType)
+	{
+	case EAsyncOverLapShape::Line:
+		CollisionShape.ShapeType = ECollisionShape::Type::Line;
+		break;
+	case EAsyncOverLapShape::Box:
+		CollisionShape.ShapeType = ECollisionShape::Type::Box;
+		break;
+	case EAsyncOverLapShape::Capsule:
+		CollisionShape.ShapeType = ECollisionShape::Type::Capsule;
+		break;
+	case EAsyncOverLapShape::Sphere:
+		CollisionShape.ShapeType = ECollisionShape::Type::Sphere;
+		break;
+	default:
+		CollisionShape.ShapeType = ECollisionShape::Type::Line;
+		break;
+	}
+	FCollisionQueryParams Params(SCENE_QUERY_STAT(UKAsyncOverLap), true);
+	Params.AddIgnoredActors(MoveTemp(AsyncOverLapInfo.InIgnoreActors));
+	
+	TraceTaskID = GetWorld()->AsyncOverlapByChannel(
+		AsyncOverLapInfo.OverLapLoaction,
+		AsyncOverLapInfo.OverLapRotator.Quaternion(),
+		AsyncOverLapInfo.CollisionChannel,
+		CollisionShape,
+		Params,
+		FCollisionResponseParams::DefaultResponseParam,
+		&OverlapDelegate);
+}
+
+void UUKAsyncOverLap::Cancel()
+{
+	Super::Cancel();
+	if (OverlapDelegate.IsBound())
+    {
+    	OverlapDelegate.Unbind();
+    }
+}
+
+void UUKAsyncOverLap::BeginDestroy()
+{
+	Super::BeginDestroy();
+
+	if (OverlapDelegate.IsBound())
+	{
+		OverlapDelegate.Unbind();
+	}
+}
+
+void UUKAsyncOverLap::AsyncOverLapFinish(const FTraceHandle& TraceHandle, FOverlapDatum& OverlapDatum)
+{
+	if(TraceTaskID != TraceHandle)
+	{
+		return;
+	}
+
+	FUKAsyncOverLapResult AsyncOverLapResult;
+	for(const FOverlapResult& Result : OverlapDatum.OutOverlaps)
+	{
+		FUkOverlapResult UkOverlapResult;
+		UkOverlapResult.OverlapActor = Result.GetActor();
+		UkOverlapResult.Component = Result.GetComponent();
+		UkOverlapResult.bBlockingHit = Result.bBlockingHit;
+		UkOverlapResult.ItemIndex = Result.ItemIndex;
+		
+		AsyncOverLapResult.OutOverlaps.Emplace(UkOverlapResult);
+	}
+	FinishPin.Broadcast(AsyncOverLapResult);
+}
+
+UUKAsyncOverLap* UUKAsyncOverLap::UKAsyncOverLap(const UObject* WorldContextObject, const FUKAsyncOverLapInfo AsyncOverLapInfo)
+{
+	UWorld* WorldContext = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::ReturnNull);
+	if(!ensureAlwaysMsgf(IsValid(WorldContext), TEXT("World Context was not valid.")))
+	{
+		return nullptr;
+	}
+	
+	UUKAsyncOverLap* Node = NewObject<UUKAsyncOverLap>();
+	Node->WorldContextObject = WorldContextObject->GetWorld();
+	Node->AsyncOverLapInfo = AsyncOverLapInfo;
+	Node->RegisterWithGameInstance(WorldContext->GetGameInstance());
+	return Node;
+}
---
+++ UETest/Source/UETest/Public/UKAsyncOverLap.h
@@ -0,0 +1,97 @@
+// Copyright Kong Studios, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/CancellableAsyncAction.h"
+#include "UKAsyncOverLap.generated.h"
+
+UENUM(BlueprintType)
+enum class EAsyncOverLapShape : uint8
+{
+	Line,
+	Box,
+	Sphere,
+	Capsule
+};
+
+USTRUCT(BlueprintType)
+struct UETEST_API FUKAsyncOverLapInfo
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FVector OverLapLoaction = FVector::ZeroVector;
+	UPROPERTY(BlueprintReadWrite)
+	FRotator OverLapRotator = FRotator::ZeroRotator;
+	UPROPERTY(BlueprintReadWrite)
+	TEnumAsByte<ECollisionChannel> CollisionChannel;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<const AActor*> InIgnoreActors;
+	UPROPERTY(BlueprintReadWrite)
+	EAsyncOverLapShape ShapeType;
+};
+
+USTRUCT(BlueprintType)
+struct UETEST_API FUkOverlapResult
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly)
+	AActor* OverlapActor;
+
+	UPROPERTY(BlueprintReadOnly)
+	UPrimitiveComponent* Component;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 ItemIndex = INDEX_NONE;
+
+	UPROPERTY(BlueprintReadOnly)
+	uint32 bBlockingHit : 1;
+};
+
+USTRUCT(BlueprintType)
+struct UETEST_API FUKAsyncOverLapResult
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FUkOverlapResult> OutOverlaps;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOverLapPin, const FUKAsyncOverLapResult&, AsyncTraceResult);
+
+/**
+ * 
+ */
+UCLASS()
+class UETEST_API UUKAsyncOverLap : public UCancellableAsyncAction
+{
+	GENERATED_BODY()
+protected:
+	virtual void Activate() override;
+	virtual void Cancel() override;
+	virtual void BeginDestroy() override;
+
+	virtual UWorld* GetWorld() const override
+	{
+		return WorldContextObject.IsValid() ? WorldContextObject.Get() : nullptr;
+	}
+
+	void AsyncOverLapFinish(const FTraceHandle& TraceHandle, FOverlapDatum& OverlapDatum);
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", ClampMin = "0", WorldContext = "WorldContextObject"), Category="AsyncNodes")
+	static UUKAsyncOverLap* UKAsyncOverLap(const UObject* WorldContextObject, const FUKAsyncOverLapInfo AsyncOverLapInfo);
+
+private:
+	FTraceHandle TraceTaskID; 
+	TWeakObjectPtr<UWorld> WorldContextObject = nullptr;
+	FOverlapDelegate OverlapDelegate;
+
+	FUKAsyncOverLapInfo AsyncOverLapInfo;
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOverLapPin FinishPin;
+};
