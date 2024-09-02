9b674eca3116d9c63a66c5b5d981b0d342321c57
jjh2v2 <jjh2v2@gmail.com>
2024-09-03 00:02:09 +0900

    수정

---
+++ UETest/Source/UETest/UI/UKWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UKWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UETEST_API UUKWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
