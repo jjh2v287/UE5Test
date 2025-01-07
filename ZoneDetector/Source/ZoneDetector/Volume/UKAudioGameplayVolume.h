37f35e496c5e0208f4b05ed30610d6fe60fc2425
jjh2v2 <jjh2v2@gmail.com>
2025-01-07 18:32:06 +0900

    수정

---
+++ ZoneDetector/Source/ZoneDetector/Volume/UKAudioGameplayVolume.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AudioGameplayVolume.h"
+#include "UKAudioGameplayVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, hidecategories = (Advanced, Attachment, Collision, Volume))
+class ZONEDETECTOR_API AUKAudioGameplayVolume : public AAudioGameplayVolume
+{
+	GENERATED_BODY()
+	
+};
