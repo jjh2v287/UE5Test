de145deab27f32a0896b8feadbae24e03b7ca34c
jjh2v2 <jjh2v2@gmail.com>
2025-01-23 16:32:52 +0900

    수정

---
+++ ZKGame/Source/ZKGame/Core/ZKActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ZKActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class ZKGAME_API AZKActor : public AActor
+{
+	GENERATED_BODY()
+public:
+	AZKActor();
+};
