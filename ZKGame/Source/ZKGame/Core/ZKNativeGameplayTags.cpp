1fe0453f341a148bcfe49ef9fa3b11f5d56b2b39
jjh2v2 <jjh2v2@gmail.com>
2025-01-31 04:19:09 +0900

    수정

--- ZKGame/Source/ZKGame/Core/ZKNativeGameplayTags.cpp
+++ ZKGame/Source/ZKGame/Core/ZKNativeGameplayTags.cpp
@@ -3,6 +3,7 @@
 
 #include "ZKNativeGameplayTags.h"
 
+UE_DEFINE_GAMEPLAY_TAG(TAG_ZK_Combat_Combo, "ZK.Combat.Combo");
 UE_DEFINE_GAMEPLAY_TAG(TAG_ZK_Combat_Attack01, "ZK.Combat.Attack01");
 UE_DEFINE_GAMEPLAY_TAG(TAG_ZK_Combat_Attack02, "ZK.Combat.Attack02");
 UE_DEFINE_GAMEPLAY_TAG(TAG_ZK_Combat_Attack03, "ZK.Combat.Attack03");
