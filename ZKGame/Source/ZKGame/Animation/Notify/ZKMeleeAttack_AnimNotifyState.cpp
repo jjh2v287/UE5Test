e230ab3e7a6b7cf925c0b6a708d38cbd9885a312
jjh2v2 <jjh2v2@gmail.com>
2025-02-10 01:18:45 +0900

    수정

--- ZKGame/Source/ZKGame/Animation/Notify/ZKMeleeAttack_AnimNotifyState.cpp
+++ ZKGame/Source/ZKGame/Animation/Notify/ZKMeleeAttack_AnimNotifyState.cpp
@@ -53,119 +53,149 @@ void UZKMeleeAttack_AnimNotifyState::TraceTest(USkeletalMeshComponent* MeshComp,
 
     // 1. 애니메이션 시간 계산
     const float SequenceLength = Animation->GetPlayLength();
-    const float ClampedTime = FMath::Fmod(CurrentMontagePosition, SequenceLength);
+	const float ClampedPreTime = FMath::Fmod(PreMontagePosition, SequenceLength);
+    const float ClampedCurrentTime  = FMath::Fmod(CurrentMontagePosition, SequenceLength);
 
-    // 2. 애니메이션 데이터 추출 준비
-    FAnimExtractContext ExtractionContext(ClampedTime, true);
-    USkeleton* Skeleton = Animation->GetSkeleton();
-    const FReferenceSkeleton& ReferenceSkeleton = Skeleton->GetReferenceSkeleton();
-    const int32 NumBones = ReferenceSkeleton.GetNum();
+	float delta = ClampedCurrentTime - ClampedPreTime;
+	float addTime = 1.0f / 60.0f;
+	int32 count = FMath::CeilToFloat((delta / addTime) + 0.0001);
+	count = FMath::Max(count, 2);
+	UE_LOG(LogAnimation,Warning, TEXT("count %d : delta/ addTime %f"), count, (delta / addTime) + 0.0001);
+	for (int32 i = 1; i < count; i++)
+	{
+		float PreAnimTime = ClampedPreTime + ((i - 1) * addTime);
+		float CurrentAnimTime = ClampedPreTime + (i * addTime);
+	
+		// 2. 애니메이션 데이터 추출 준비
+		FAnimExtractContext ExtractionPreContext(PreAnimTime , true);
+		FAnimExtractContext ExtractionCurrentContext(CurrentAnimTime , true);
+		USkeleton* Skeleton = Animation->GetSkeleton();
+		const FReferenceSkeleton& ReferenceSkeleton = Skeleton->GetReferenceSkeleton();
+		const int32 NumBones = ReferenceSkeleton.GetNum();
 
-    // 3. 본 컨테이너 설정
-    static TMap<FReferenceSkeleton*, FBoneContainer> BoneContainerCache;
-    FBoneContainer BoneContainer;
+		// 3. 본 컨테이너 설정
+		static TMap<FReferenceSkeleton*, FBoneContainer> BoneContainerCache;
+		FBoneContainer BoneContainer;
     
-    // 캐시된 본 컨테이너가 있으면 사용
-    if (const FBoneContainer* CachedBoneContainer = BoneContainerCache.Find(&ReferenceSkeleton))
-    {
-        BoneContainer = *CachedBoneContainer;
-    }
-    else
-    {
-        // 새로운 본 컨테이너 생성
-        TArray<uint16> BoneIndices;
-        BoneIndices.SetNumUninitialized(NumBones);
-        for (int32 Index = 0; Index < NumBones; ++Index)
-        {
-            BoneIndices[Index] = static_cast<uint16>(Index);
-        }
+		// 캐시된 본 컨테이너가 있으면 사용
+		if (const FBoneContainer* CachedBoneContainer = BoneContainerCache.Find(&ReferenceSkeleton))
+		{
+			BoneContainer = *CachedBoneContainer;
+		}
+		else
+		{
+			// 새로운 본 컨테이너 생성
+			TArray<uint16> BoneIndices;
+			BoneIndices.SetNumUninitialized(NumBones);
+			for (int32 Index = 0; Index < NumBones; ++Index)
+			{
+				BoneIndices[Index] = static_cast<uint16>(Index);
+			}
         
-        BoneContainer.SetUseRAWData(true);
-        BoneContainer.InitializeTo(BoneIndices, UE::Anim::FCurveFilterSettings(), *Skeleton);
-        BoneContainerCache.Emplace(&(const_cast<FReferenceSkeleton&>(ReferenceSkeleton)), BoneContainer);
-    }
+			BoneContainer.SetUseRAWData(true);
+			BoneContainer.InitializeTo(BoneIndices, UE::Anim::FCurveFilterSettings(), *Skeleton);
+			BoneContainerCache.Emplace(&(const_cast<FReferenceSkeleton&>(ReferenceSkeleton)), BoneContainer);
+		}
 
-    // 4. 포즈 데이터 준비
-    FCompactPose OutPose;
-    OutPose.SetBoneContainer(&BoneContainer);
-    FBlendedCurve OutCurve;
-    OutCurve.InitFrom(BoneContainer);
-    UE::Anim::FStackAttributeContainer TempAttributes;
-    FAnimationPoseData PoseData{OutPose, OutCurve, TempAttributes};
+		// 4. 포즈 데이터 준비
+		FCompactPose OutPrePose;
+		OutPrePose.SetBoneContainer(&BoneContainer);
+		FCompactPose OutCurrentPose;
+		OutCurrentPose.SetBoneContainer(&BoneContainer);
+		FBlendedCurve OutCurve;
+		OutCurve.InitFrom(BoneContainer);
+		UE::Anim::FStackAttributeContainer TempAttributes;
+		FAnimationPoseData PosePreData{OutPrePose, OutCurve, TempAttributes};
+		FAnimationPoseData PoseCurrentData{OutCurrentPose, OutCurve, TempAttributes};
 
-    // 5. 애니메이션 포즈 추출
-    if (UAnimMontage* AnimMontage = Cast<UAnimMontage>(Animation))
-    {
-        AnimMontage->SlotAnimTracks[0].AnimTrack.GetAnimationPose(PoseData, ExtractionContext);
-    }
-    else
-    {
-        Animation->GetAnimationPose(PoseData, ExtractionContext);
-    }
+		// 5. 애니메이션 포즈 추출
+		if (UAnimMontage* AnimMontage = Cast<UAnimMontage>(Animation))
+		{
+			AnimMontage->SlotAnimTracks[0].AnimTrack.GetAnimationPose(PosePreData, ExtractionPreContext);
+			AnimMontage->SlotAnimTracks[0].AnimTrack.GetAnimationPose(PoseCurrentData, ExtractionCurrentContext);
+		}
+		else
+		{
+			Animation->GetAnimationPose(PosePreData, ExtractionPreContext);
+			Animation->GetAnimationPose(PoseCurrentData, ExtractionCurrentContext);
+		}
 
-    // 6. 본 트랜스폼 계산
-    FName SocketBoneName = MeshComp->GetSocketBoneName(BoneName);
-    int32 BoneIndex = ReferenceSkeleton.FindBoneIndex(SocketBoneName);
-    if (BoneIndex == INDEX_NONE)
-    {
-        BoneIndex = 0;
-        UE_LOG(LogTemp, Warning, TEXT("Bone '%s' not found in Skeleton."), *BoneName.ToString());
-    }
+		// 6. 본 트랜스폼 계산
+		FName SocketBoneName = MeshComp->GetSocketBoneName(BoneName);
+		int32 BoneIndex = ReferenceSkeleton.FindBoneIndex(SocketBoneName);
+		if (BoneIndex == INDEX_NONE)
+		{
+			BoneIndex = 0;
+			UE_LOG(LogTemp, Warning, TEXT("Bone '%s' not found in Skeleton."), *BoneName.ToString());
+		}
 
-    // 부모 본 체인 구성
-    int32 ParentIndex = BoneIndex;
-    TArray<int32> ParentBoneIndexes;
-    while (ParentIndex != INDEX_NONE)
-    {
-        ParentBoneIndexes.Emplace(ParentIndex);
-        ParentIndex = ReferenceSkeleton.GetParentIndex(ParentIndex);
-    }
-    Algo::Reverse(ParentBoneIndexes);
+		// 부모 본 체인 구성
+		int32 ParentIndex = BoneIndex;
+		TArray<int32> ParentBoneIndexes;
+		while (ParentIndex != INDEX_NONE)
+		{
+			ParentBoneIndexes.Emplace(ParentIndex);
+			ParentIndex = ReferenceSkeleton.GetParentIndex(ParentIndex);
+		}
+		Algo::Reverse(ParentBoneIndexes);
 
-    // 7. 최종 트랜스폼 계산
-    FTransform OutTransform = FTransform::Identity;
-    FTransform RelativeTransform = MeshComp->GetComponentTransform().GetRelativeTransform(Owner->GetTransform());
-    OutTransform = RelativeTransform * OutTransform;
+		// 7. 최종 트랜스폼 계산
+		FTransform RelativeTransform = MeshComp->GetComponentTransform().GetRelativeTransform(Owner->GetTransform());
+		FTransform PreTransform = FTransform::Identity;
+		FTransform CurrentTransform = FTransform::Identity;
+		PreTransform = RelativeTransform * CurrentTransform;
+		CurrentTransform = RelativeTransform * CurrentTransform;
     
-    for (int32 ParentBoneIndex : ParentBoneIndexes)
-    {
-        if (ParentBoneIndex == 0)
-            continue;
+		for (int32 ParentBoneIndex : ParentBoneIndexes)
+		{
+			if (ParentBoneIndex == 0)
+			{
+				// root 애니메이션이라 root 본은 패스
+				continue;
+			}
         
-        const FCompactPoseBoneIndex CompactIndex = BoneContainer.MakeCompactPoseIndex(FMeshPoseBoneIndex(ParentBoneIndex));
-        FTransform LocalBoneTransform = PoseData.GetPose()[CompactIndex];
-        OutTransform = LocalBoneTransform * OutTransform;
-    }
+			const FCompactPoseBoneIndex CompactIndex = BoneContainer.MakeCompactPoseIndex(FMeshPoseBoneIndex(ParentBoneIndex));
+			FTransform LocalPreBoneTransform = PosePreData.GetPose()[CompactIndex];
+			PreTransform = LocalPreBoneTransform * PreTransform;
+			FTransform LocalCurrentBoneTransform = PoseCurrentData.GetPose()[CompactIndex];
+			CurrentTransform = LocalCurrentBoneTransform * CurrentTransform;
+		}
 
-    // 8. 월드 공간 변환
-    FVector StartFinalBoneTransform = Owner->GetActorRotation().RotateVector(OutTransform.GetLocation());
-    StartFinalBoneTransform = StartFinalBoneTransform + Owner->GetTransform().GetLocation();
+		// 8. 월드 공간 변환
+		FVector StartBoneTransform = Owner->GetActorRotation().RotateVector(PreTransform.GetLocation());
+		StartBoneTransform = StartBoneTransform + Owner->GetTransform().GetLocation();
+		FVector EndBoneTransform = Owner->GetActorRotation().RotateVector(CurrentTransform.GetLocation());
+		EndBoneTransform = EndBoneTransform + Owner->GetTransform().GetLocation();
 
-    // 9. 충돌 검사 실행
-    TArray<FHitResult> HitResults;
-    TArray<AActor*> ActorsToIgnore = {Owner};
-    bool bHit = UKismetSystemLibrary::CapsuleTraceMulti(
-        Owner,
-        StartFinalBoneTransform,
-        StartFinalBoneTransform,
-        1.f,  // 캡슐 반경
-        1.f,  // 캡슐 높이
-        ETraceTypeQuery::TraceTypeQuery1,
-        false,  // 복잡한 충돌 체크 비활성화
-        ActorsToIgnore,
-        EDrawDebugTrace::Type::ForDuration,
-        HitResults,
-        true,  // 충돌 발생 시 트레이스 중단
-        FLinearColor::Red,
-        FLinearColor::Blue,
-        5.f  // 디버그 표시 지속 시간
-    );
+		// UE_LOG(LogAnimation,Warning, TEXT("StartBoneTransform X : %f , Y : %f , Z : %f"), StartBoneTransform.X, StartBoneTransform.Y, StartBoneTransform.Z);
+		// UE_LOG(LogAnimation,Warning, TEXT("EndBoneTransform X : %f , Y : %f , Z : %f"), EndBoneTransform.X, EndBoneTransform.Y, EndBoneTransform.Z);
+	
+		// 9. 충돌 검사 실행
+		TArray<FHitResult> HitResults;
+		TArray<AActor*> ActorsToIgnore = {Owner};
+		bool bHit = UKismetSystemLibrary::CapsuleTraceMulti(
+			Owner,
+			StartBoneTransform,
+			EndBoneTransform,
+			2.f,  // 캡슐 반경
+			2.f,  // 캡슐 높이
+			ETraceTypeQuery::TraceTypeQuery1,
+			false,  // 복잡한 충돌 체크 비활성화
+			ActorsToIgnore,
+			EDrawDebugTrace::Type::ForDuration,
+			HitResults,
+			true,  // 충돌 발생 시 트레이스 중단
+			FLinearColor::Red,
+			FLinearColor::Blue,
+			20.0f  // 디버그 표시 지속 시간
+		);
 
-    // TODO: 충돌 처리 로직 추가
-    if (bHit)
-    {
-        // 충돌 이벤트 처리
-    }
+		// TODO: 충돌 처리 로직 추가
+		if (bHit)
+		{
+			// 충돌 이벤트 처리
+		}
+	}
 }
 
 /*void UZKMeleeAttack_AnimNotifyState::LegacyTrace(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
