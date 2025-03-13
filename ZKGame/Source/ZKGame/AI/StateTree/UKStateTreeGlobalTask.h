d3eb8b493364345ef227b58232f48dc6fc9e7403
jjh2v2 <jjh2v2@gmail.com>
2025-03-13 14:29:47 +0900

    수정

--- ZKGame/Source/ZKGame/AI/StateTree/UKStateTreeGlobalTask.h
+++ ZKGame/Source/ZKGame/AI/StateTree/UKStateTreeGlobalTask.h
@@ -57,8 +57,7 @@ struct ZKGAME_API FUKStateTreeGlobalTask : public FStateTreeTaskCommonBase
 
 	// using FInstanceDataType = FUKStateTreeGlobalTaskData;
 	// virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
-	using UInstanceDataType = UUKGlobalTaskData;
-	virtual const UStruct* GetInstanceDataType() const override { return UInstanceDataType::StaticClass(); };
+	virtual const UStruct* GetInstanceDataType() const override { return UUKGlobalTaskData::StaticClass(); };
 	
 	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
 	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
