3738cad005bc319eade455bcad5adb44bb532e98
jjh2v2 <jjh2v2@gmail.com>
2025-04-09 14:52:21 +0900

    수정

---
+++ FlecsTest/Source/FlecsTest/FlecsSubsystem.cpp
@@ -0,0 +1,106 @@
+#include "FlecsSubsystem.h"
+
+#include "Containers/Ticker.h"
+#include "Engine/World.h"
+flecs::world* UFlecsSubsystem::GetEcsWorld() const{return ECSWorld;}
+void UFlecsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	OnTickDelegate = FTickerDelegate::CreateUObject(this, &UFlecsSubsystem::Tick);
+	OnTickHandle = FTSTicker::GetCoreTicker().AddTicker(OnTickDelegate);
+	
+	//sets title in Flecs Explorer
+  char name[] = { "Minimum Viable Flecs" };
+	char* argv = name;
+	ECSWorld = new flecs::world(1, &argv);
+	
+	//flecs explorer and monitor
+	//comment this out if you not using it, it has some performance overhead
+	//go to https://www.flecs.dev/explorer/ when the project is running to inspect active entities and values
+	GetEcsWorld()->import<flecs::monitor>();
+	GetEcsWorld()->set<flecs::Rest>({});
+	
+	//expose values with names to Flecs Explorer for easier inspection & debugging
+	GetEcsWorld()->component<FlecsCorn>().member<float>("Current Growth");
+	GetEcsWorld()->component<FlecsISMIndex>().member<int>("ISM Render index");	
+	
+	UE_LOG(LogTemp, Warning, TEXT("UUnrealFlecsSubsystem has started!"));
+	Super::Initialize(Collection);
+}
+
+void UFlecsSubsystem::InitFlecs(UStaticMesh* InMesh)
+{
+	//Spawn an actor and add an Instanced Static Mesh component to it.
+	//This will render our entities.
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	CornRenderer = Cast<UInstancedStaticMeshComponent>((GetWorld()->SpawnActor(AActor::StaticClass(), &FVector::ZeroVector, &FRotator::ZeroRotator, SpawnInfo))->AddComponentByClass(UInstancedStaticMeshComponent::StaticClass(), false, FTransform(FVector::ZeroVector), false));
+	CornRenderer->SetStaticMesh(InMesh);
+	CornRenderer->bUseDefaultCollision = false;
+	CornRenderer->SetGenerateOverlapEvents(false);
+	CornRenderer->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	CornRenderer->SetCanEverAffectNavigation(false);
+	CornRenderer->NumCustomDataFloats = 2;
+	
+	//this system processes the growth of our entities
+	auto system_grow = GetEcsWorld()->system<FlecsCorn>("Grow System")
+	.iter([](flecs::iter it, FlecsCorn* fc) {
+		float GrowthRate = 20*it.delta_time();
+		for (int i : it) {
+			//if we haven't grown fully (100) then grow
+			fc[i].Growth+=(fc[i].Growth<100)*GrowthRate;
+		}
+	});
+	
+	//this system sets the growth value of our entities in ISM so we can access it from materials.
+	auto system_copy_growth = GetEcsWorld()->system<FlecsCorn, FlecsISMIndex, FlecsIsmRef>("Grow Renderer System")
+	.iter([](flecs::iter it, FlecsCorn* fw, FlecsISMIndex* fi, FlecsIsmRef* fr) {
+		for (int i : it) {
+			auto index = fi[i].Value;
+			fr[i].Value->SetCustomDataValue(index, 0, fw[i].Growth, true);
+		}
+	});
+	
+	UE_LOG(LogTemp, Warning, TEXT("Flecs Corn system initialized!"));
+}
+
+void UFlecsSubsystem::Deinitialize()
+{
+	FTSTicker::GetCoreTicker().RemoveTicker(OnTickHandle);
+	
+	if (ECSWorld)
+	{
+		delete ECSWorld;
+		ECSWorld = nullptr;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("UUnrealFlecsSubsystem has shut down!"));
+	Super::Deinitialize();
+}
+
+FFlecsEntityHandle UFlecsSubsystem::SpawnCornEntity(FVector location, FRotator rotation)
+{
+	auto IsmID = CornRenderer->AddInstance(FTransform(rotation, location));
+	auto entity = GetEcsWorld()->entity()
+	.set<FlecsIsmRef>({CornRenderer})
+	.set<FlecsISMIndex>({IsmID})
+	.set<FlecsCorn>({0})
+	.child_of<Corns>()
+	.set_name(StringCast<ANSICHAR>(*FString::Printf(TEXT("Corn%d"), IsmID)).Get());
+	return FFlecsEntityHandle{int(entity.id())};
+}
+
+void UFlecsSubsystem::SetEntityHighlight(FFlecsEntityHandle entityHandle, bool isHighlighted)
+{
+	int idx = GetEcsWorld()->entity(entityHandle.FlecsEntityId).get<FlecsISMIndex>()->Value;
+	CornRenderer->SetCustomDataValue(idx, 1, (float)isHighlighted, true);
+}
+float UFlecsSubsystem::GetEntityGrowthData(FFlecsEntityHandle entityHandle)
+{
+	return GetEcsWorld()->entity(entityHandle.FlecsEntityId).get<FlecsCorn>()->Growth;
+}
+
+bool UFlecsSubsystem::Tick(float DeltaTime)
+{
+	if(ECSWorld) ECSWorld->progress(DeltaTime);
+	return true;
+}
---
+++ FlecsTest/Source/FlecsTest/FlecsSubsystem.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "flecs.h"
+#include "CoreMinimal.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Containers/Ticker.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FlecsSubsystem.generated.h"
+
+struct FlecsTransform
+{
+	FTransform Value;
+};
+struct FlecsISMIndex
+{
+	int Value;
+};
+struct FlecsIsmRef
+{
+	UInstancedStaticMeshComponent* Value;
+};
+struct FlecsCorn
+{
+	float Growth;
+};
+struct Corns {};
+
+USTRUCT(BlueprintType)
+struct FFlecsEntityHandle
+{
+	GENERATED_USTRUCT_BODY()
+	FFlecsEntityHandle()  {}
+	FFlecsEntityHandle(int inId)
+	{
+		FlecsEntityId = inId;
+	}
+	UPROPERTY(BlueprintReadWrite)
+	int FlecsEntityId;
+};
+
+UCLASS()
+class FLECSTEST_API UFlecsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	flecs::world* GetEcsWorld() const;
+	
+	UPROPERTY(EditAnywhere)
+	UInstancedStaticMeshComponent* CornRenderer = nullptr;
+
+	
+	UFUNCTION(BlueprintCallable, Category="FLECS")
+	void InitFlecs(UStaticMesh* InMesh);
+	UFUNCTION(BlueprintCallable, Category="FLECS")
+	FFlecsEntityHandle SpawnCornEntity(FVector location, FRotator rotation);
+	UFUNCTION(BlueprintCallable, Category="FLECS")
+	void SetEntityHighlight(FFlecsEntityHandle entityHandle, bool isHighlighted);
+	UFUNCTION(BlueprintCallable, Category="FLECS")
+	float GetEntityGrowthData(FFlecsEntityHandle entityHandle);
+
+protected:
+	FTickerDelegate OnTickDelegate;
+	FTSTicker::FDelegateHandle OnTickHandle;
+	flecs::world* ECSWorld = nullptr;
+private:
+	bool Tick(float DeltaTime);
+	
+};
