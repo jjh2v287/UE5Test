e9b42783659129b2894e826ea2778312e8c510ea
jjh2v2 <jjh2v2@gmail.com>
2025-07-16 00:48:58 +0900

    수정

---
+++ DarkHon/Source/DarkHon/Faction/FactionComponent.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FactionComponent.h"
+
+
+UFactionComponent::UFactionComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UFactionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	// 데이터에서 팩션 설정
+	// Faction = 
+}
+
+int32 FFactionDataTable::GetThreatLevelTowards(AActor* SourceActor, AActor* OtherActor)
+{
+	if (!SourceActor || !OtherActor)
+	{
+		return DefaultThreat;
+	}
+
+	UFactionComponent* SourceFactionComponent = SourceActor->FindComponentByClass<UFactionComponent>();
+	if (!SourceFactionComponent)
+	{
+		return DefaultThreat;
+	}
+
+	UFactionComponent* OtherFactionComponent = OtherActor->FindComponentByClass<UFactionComponent>();
+	if (!OtherFactionComponent)
+	{
+		return DefaultThreat;
+	}
+
+	// 팩션 대 팩션으로 위협도를 조회합니다.
+	const EFaction SourceFaction = SourceFactionComponent->GetFaction();
+	const EFaction OtherFaction = SourceFactionComponent->GetFaction();
+	return QueryThreatLevel(SourceFaction, OtherFaction);
+}
+
+int32 FFactionDataTable::QueryThreatLevel(EFaction SourceFaction, EFaction TargetFaction)
+{
+	if (SourceFaction == TargetFaction)
+	{
+		return DefaultThreat;
+	}
+	
+	/*if (!FactionDataTable )
+	{
+		return DefaultThreat;
+	}
+    
+	// Enum 값을 문자열로 변환하여 행 이름으로 사용합니다.
+	const FString ContextString(TEXT("Faction Data Context"));
+	const FName RowName = *UEnum::GetValueAsString(SourceFaction);
+
+	// 데이터 테이블에서 SourceFaction에 해당하는 행을 찾습니다.
+	const FFactionDataTable* RowData = FactionDataTable->FindRow<FFactionDataTable>(RowName, ContextString);
+
+	if (RowData)
+	{
+		// 행 데이터의 ThreatMap에서 TargetFaction에 대한 위협도를 찾습니다.
+		if (const float* FoundThreat = RowData->Faction.Find(TargetFaction))
+		{
+			return *FoundThreat;
+		}
+	}*/
+    
+	// 행이나 맵에 값이 없는 경우 기본값을 반환합니다.
+	return DefaultThreat;
+}
---
+++ DarkHon/Source/DarkHon/Faction/FactionComponent.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FactionComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EFaction : uint8
+{
+	Player      UMETA(DisplayName = "Player"),
+	Goblin      UMETA(DisplayName = "Goblin"),
+	Slime       UMETA(DisplayName = "Slime"),
+	Test		UMETA(DisplayName = "Test"),
+	Orc			UMETA(DisplayName = "Orc")
+};
+
+USTRUCT(BlueprintType)
+struct FFactionDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Faction Type")
+	EFaction Faction = EFaction::Player;
+	
+	// 이 팩션이 다른 팩션에 대해 느끼는 위협도를 저장하는 맵
+	// Key: 상대방 팩션, Value: 위협도 (높을수록 적대적)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Threat Map")
+	TMap<EFaction, int32> ThreatMap;
+
+	// 다른 액터에 대한 위협도를 반환합니다.
+	static int32 GetThreatLevelTowards(AActor* SourceActor, AActor* OtherActor);
+
+	// 특정 팩션에 대한 위협도를 반환합니다.
+	static int32 QueryThreatLevel(EFaction SourceFaction, EFaction TargetFaction);
+
+private:
+	// 기본 위협도 (데이터 테이블에 명시되지 않은 경우)
+	static constexpr int32 DefaultThreat = 0;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DARKHON_API UFactionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFactionComponent();
+	virtual void BeginPlay() override;
+
+	EFaction GetFaction() const
+	{
+		return Faction;
+	}
+	
+protected:
+	EFaction Faction = EFaction::Player;
+};
