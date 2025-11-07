aaf6d38882e2e40af90a80d60c43aaa7cdd286b1
jjh2v2 <jjh2v2@gmail.com>
2025-11-07 14:49:12 +0900

    수정

---
+++ SkeletalMeshes/Source/SkeletalMeshes/SkeletalMeshes.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkeletalMeshes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkeletalMeshes, "SkeletalMeshes" );
+
+DEFINE_LOG_CATEGORY(LogSkeletalMeshes)
