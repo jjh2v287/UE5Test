63ad999a995adc2cf4319c059ffe1eda3921d878
jjh2v2 <jjh2v2@gmail.com>
2025-11-18 16:57:35 +0900

    수정

---
+++ NPC/UKNPC/Source/UKNPC/UKNPC.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UKNPC : ModuleRules
+{
+	public UKNPC(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"UKNPC",
+			"UKNPC/Variant_Platforming",
+			"UKNPC/Variant_Platforming/Animation",
+			"UKNPC/Variant_Combat",
+			"UKNPC/Variant_Combat/AI",
+			"UKNPC/Variant_Combat/Animation",
+			"UKNPC/Variant_Combat/Gameplay",
+			"UKNPC/Variant_Combat/Interfaces",
+			"UKNPC/Variant_Combat/UI",
+			"UKNPC/Variant_SideScrolling",
+			"UKNPC/Variant_SideScrolling/AI",
+			"UKNPC/Variant_SideScrolling/Gameplay",
+			"UKNPC/Variant_SideScrolling/Interfaces",
+			"UKNPC/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
